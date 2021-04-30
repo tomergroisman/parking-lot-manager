ff175c3ca0c18d91a6c6334ea42f4ae8d1904531
Tomer Groisman <tomergroisman@gmail.com>
2021-04-30 18:36:34 +0300

    Change charge fees to 15 min intervals

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,8 @@ const Database = require('../database');
 const db = new Database();
 
 const HOURLY_CHARGE = 10;
+const INCREMENT_INTERVAL = 15;
+const PERIODIC_CHARGE = HOURLY_CHARGE / (60 / INCREMENT_INTERVAL);
 
 const millisToMinutes = (millis) => {
     return millis / 60000;
@@ -24,12 +26,12 @@ const controllers = {
         const timestamp = Date.now();
         const parking = db.remove(parkingId);
         const minutesSpent = millisToMinutes(timestamp - parking.timestamp);
-        const charge = (Math.floor(minutesSpent) % 60 + 1) * HOURLY_CHARGE 
+        const charge = Math.ceil(minutesSpent / 15) * PERIODIC_CHARGE
         res.json({
             plate: parking.plate,
             totalParkingTime: minutesSpent.toFixed(1),
             parkingLot: parking.parkingLot,
-            charge
+            charge: charge % 1 == 0 ? charge : charge.toFixed(1),
         });
     },
     getAllParkings: (req, res) => {
